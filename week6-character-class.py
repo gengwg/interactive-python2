0d3be3138edb10a64447ae79bffc7bbc1f4945d1
Weigang Geng <gengwg@gmail.com>
2016-08-16 22:07:34 -0700

    character class example

---
+++ week6-character-class.py
@@ -0,0 +1,30 @@
+class Character:
+    # define some interfaces
+    def __init__(self, name, initial_health):
+        self.name = name
+        self.health = initial_health
+        self.inventory = []
+
+    def __str__(self):
+        s  = "Name: " + self.name
+        s += " Health: " + str(self.health)
+        s += " Inventory: " + str(self.inventory)
+        return s
+
+    def grab(self, item):
+        self.inventory.append(item)
+
+    def get_health(self):
+        return self.health
+
+def example():
+    # use the interfaces
+    me = Character("Bob", 20)
+    print str(me)
+    me.grab("pencil")
+    me.grab("paper")
+    print str(me)
+    print "Health:", me.get_health()
+
+example()
+
